8707021b0bae83b3569972fb3caa48be12ec617c
Prince <Prince@moonraft.com>
2020-04-07 22:29:29 +0530

    Updates

---
+++ precache-manifest.ff865f2193d9a23af75a7151711b5b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c12c1d28e6b178e6127c3e45dcc062",
+    "url": "/react-first-ass/index.html"
+  },
+  {
+    "revision": "7ff2f36fc3b14519582a",
+    "url": "/react-first-ass/static/css/main.1153ba58.chunk.css"
+  },
+  {
+    "revision": "968d7b17324f7829b693",
+    "url": "/react-first-ass/static/js/2.764eac27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first-ass/static/js/2.764eac27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff2f36fc3b14519582a",
+    "url": "/react-first-ass/static/js/main.f72092e8.chunk.js"
+  },
+  {
+    "revision": "55282a390f92bf756e4e",
+    "url": "/react-first-ass/static/js/runtime-main.782e8063.js"
+  },
+  {
+    "revision": "a0e4fa0a928267d9ee27d0022777c0c5",
+    "url": "/react-first-ass/static/media/developer.a0e4fa0a.PNG"
+  }
+]);
