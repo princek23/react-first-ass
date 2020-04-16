2ed452182cfd4e26e3da1efb1246ae77a7c1df0a
Prince <Prince@moonraft.com>
2020-04-16 13:30:12 +0530

    app.js changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <h1>My First React App</h1>
       <h2>Author: Prince</h2>
       <img src={profileImage} alt="profile-image"/>
     </div>
